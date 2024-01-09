0cbf2df71a8942b390029f4c018c7ceb586983c1
g-barrett <barrettg516@gmail.com>
2024-01-09 17:06:21 -0600

    outline html, api key and base url to js

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+const APIKey = '607f916d5e35cb2ff44c2fc0e4c26499';
+const baseURL = 'https://api.openweathermap.org/data/2.5/forecast'
+
+
+
+// format for url queries: ?lat={lat}&lon={lon}&appid={API key}
+
+
